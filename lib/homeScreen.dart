6353a79f9fe9d0248975a597ad37f291525c7595
Ashutosh <ashu.ak1112@gmail.com>
2021-06-05 12:11:13 +0530

    Base Model

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -15,40 +15,73 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Colors.black26,
+        backgroundColor: Colors.white,
         body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors:[
-                Colors.green,
-                Colors.greenAccent
-              ]
-            )
-          ),
           child: Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Container(
-                height: MediaQuery.of(context).size.height*0.7,
+                height: MediaQuery.of(context).size.height*0.5,
                 decoration: BoxDecoration(
                   image: DecorationImage(
-                    image: AssetImage('assets/images/home.jpg'),
+                    image: AssetImage('assets/images/Home2.jpg'),
                         fit: BoxFit.cover,
                     // colorFilter: ColorFilter.mode(Colors.black45, BlendMode.darken)
                   ),
                   borderRadius: BorderRadius.only(bottomLeft: Radius.circular(100.0)),
                 ),
               ),
-              MaterialButton(
-                child: Text("Take Photo",style: TextStyle(color: Colors.deepOrange,fontSize: 25.0),),
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                color: Colors.greenAccent,
-
+              SizedBox(
+                height: MediaQuery.of(context).size.height*0.1,
               ),
               MaterialButton(
-                child: Text("Select Photo",style: TextStyle(color: Colors.deepOrange,fontSize: 25.0),),
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                color: Colors.greenAccent,
-              )
+                onPressed: (){},
+                  elevation: 10.0,
+                  focusColor: Colors.amber,
+                color: Colors.deepOrangeAccent,
+                child: Container(child: Text("Take Photo",style: TextStyle(color: Colors.white,fontSize: 25.0),
+                ),
+                  padding: EdgeInsets.only(left: 40.0,right: 40.0,top: 10.0,bottom: 10.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.deepPurple,
+                        Colors.deepOrangeAccent
+                      ]
+                    )
+                  ),
+               ),
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                padding: EdgeInsets.all(0.0)
+              ),
+              SizedBox(
+                height:50,
+              ),
+              MaterialButton(
+                  onPressed: (){},
+                  elevation: 10.0,
+                  focusColor: Colors.amber,
+                  color: Colors.deepOrangeAccent,
+                  child: Container(child: Text("Select Photo",style: TextStyle(color: Colors.white,fontSize: 25.0),
+                  ),
+                    padding: EdgeInsets.only(left: 40.0,right: 40.0,top: 10.0,bottom: 10.0),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                        gradient: LinearGradient(
+                            colors: [
+                              Colors.deepPurple,
+                              Colors.deepOrangeAccent
+                            ]
+                        )
+                    ),
+                  ),
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                  padding: EdgeInsets.all(0.0)
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height*0.1,
+              ),
             ],
           ),
         ),
